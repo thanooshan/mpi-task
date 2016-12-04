45ec8a07d1d25a5b67d666e6e70032679f80b240
thanooshan <gtshan10@gmail.com>
2016-12-04 15:59:49 +0530

    decrypt_password function returns decrypted passwords

--- serial/pwcrack2016.c
+++ serial/pwcrack2016.c
@@ -24,7 +24,7 @@ const int ENCRYPTED_SIZE = 94;
 
 void decrypt_password(int, char*, char**);
 
-
+//This function returns decrypted passwords
 void decrypt_password(const int password_length, char* password, char** plain) {
     int possibilties = pow(ALPHABET_SIZE, password_length);
     char candidates[possibilties][password_length + 1];
